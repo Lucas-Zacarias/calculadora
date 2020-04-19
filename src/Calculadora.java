8f66e4bb28ac0b292861a78c6d0146753aaf93ad
Lucas Zacarias <lucas21111999@gmail.com>
2020-04-19 17:33:24 -0300

    Se aplicaron los metodos suma y resta, y validacion del ingreso de operacion

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -4,12 +4,12 @@ public class Calculadora {
 	public static void main(String[] args) {
 		Integer a = 0;
 		Integer b = 0;
-		Integer opcion;
+		Integer opcion = 0;
 		Integer resultado = 0;
 
 		Scanner teclado = new Scanner(System.in);
 
-		do {
+		/*do {
 			System.out.println("Ingrese su operaciÃ³n" + '\n' + "1 Para restar" + '\n' + "2 Para sumar " + '\n'
 					+ "3 Para dividir" + '\n' + "4 Para multiplicar");
 
@@ -20,18 +20,54 @@ public class Calculadora {
 		a = teclado.nextInt();
 		System.out.println("Ingrese el segundo numero");
 		b = teclado.nextInt();
-	
+
 		switch(opcion){
-			case 1: resultado = a-b;
+			case 1: resultado = resta(a,b);
 					break;
-			case 2: resultado = a+b;
+			case 2: resultado = suma(a,b);
 					break;
 			case 3: resultado = a/b;
 					break;
 			case 4: resultado = a*b;
 					break;
-		}
+					
+		}*/
+		System.out.println("Ingrese su operaciÃ³n" + '\n' + "1 Para restar" + '\n' + "2 Para sumar " + '\n'
+				+ "3 Para dividir" + '\n' + "4 Para multiplicar");
+
+		opcion = teclado.nextInt();
+		if(opcion >= 1 && opcion <=4) {
+			System.out.println("Ingrese el primer numero");
+			a = teclado.nextInt();
+			System.out.println("Ingrese el segundo numero");
+			b = teclado.nextInt();
+
+			switch(opcion){
+				case 1: resultado = resta(a,b);
+						break;
+				case 2: resultado = suma(a,b);
+						break;
+				case 3: resultado = a/b;
+						break;
+				case 4: resultado = a*b;
+						break;
+						
+			}System.out.println("El resultado es " + resultado.toString());
+			}else
+			{
+				System.out.println("Opción no válida");
+			}
 		
-		System.out.println("El resultado es " + resultado.toString());
+		
+		//System.out.println("El resultado es " + resultado.toString());
+	}
+	
+	private static Integer suma (Integer nro1,Integer nro2) {
+		Integer suma = new Integer(nro1+nro2);
+		return suma; 
+	}
+	private static Integer resta(Integer nro1, Integer nro2) {
+		Integer resta = new Integer(nro1-nro2);
+		return resta; 
 	}
 }
